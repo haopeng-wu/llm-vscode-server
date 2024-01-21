62842e8ac5064ac2acf341e019a47dab2e8e46c5
HopeWu <30311646+HopeWu@users.noreply.github.com>
2024-01-21 22:43:28 +0000

    standalone with bug

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.8
-WORKDIR /src
-ADD requirements.txt requirements.txt
-ADD . /src
+WORKDIR /app
+ADD . /app
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
-CMD gunicorn --workers=2 main:app -b 127.0.0.1:5000
+# CMD gunicorn --workers=2 main:app -b 127.0.0.1:5000
+CMD python main.py
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_restful import reqparse, abort, Api, Resource
 from langchain.chat_models import AzureChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage
+from gunicorn.app.base import BaseApplication
 import yaml
 
 import logging
@@ -93,5 +94,22 @@ api.add_resource(Generate, '/generate', '/')
 def run():
     app.run(debug=True)
 
-if __name__ == '__main__':
-    run()
+class StandaloneRandomNumberAPI(BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+    def load_config(self):
+            config = {key: value for key, value in self.options.items()
+                    if key in self.cfg.settings and value is not None}
+            for key, value in config.items():
+                self.cfg.set(key.lower(), value)
+    def load(self):
+            return self.application
+
+if __name__ == "__main__":
+    options = {
+        'bind': '%s:%s' % ('127.0.0.1', '7000'),
+        'workers': 2,
+    }
+    StandaloneRandomNumberAPI(app, options).run()
