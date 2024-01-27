6d3edfecc5a1262224e3aa31892db20063b2bd20
HopeWu <30311646+HopeWu@users.noreply.github.com>
2024-01-27 22:01:02 +0000

    update prompts

--- llm.py
+++ llm.py
@@ -71,7 +71,6 @@ class LLM:
             with {self.START_TOKEN} and ends by {self.MID_TOKEN}. Insert code right after {self.MID_TOKEN}.
             The inserted code will be used in place in a code editor."""
         prompt_template = PromptTemplate.from_template(
-            prompt_context + "Only output the inserted code. The code snippet is delimited by ```. \n\n```{code_context}```",
-            input_variables=["code_context"])
-        chain = prompt_template | self.model
+            prompt_context + "Only output the inserted code. The code snippet is the following, delimited by ```. \n\n```{code_context}```")
+        chain = prompt_template | self.model 
         return chain.invoke({"code_context": code_context})
